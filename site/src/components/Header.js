d8a13afabca3d6e192bd473d9c7387a26d396888
Ryzon3 <ryanmcole20@gmail.com>
2022-08-31 14:10:28 -0400

    Added image and text to home page

--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -6,17 +6,15 @@ const Header = () => {
         <>
             <nav>
                 <ul className="headerWrapper">
-                    <div className="headerCont">
-                        <li className="headerItem">
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li className="headerItem">
-                            <Link to="/projects">Projects</Link>
-                        </li>
-                        <li className="headerItem">
-                            <Link to="/404">404</Link>
-                        </li>
-                    </div>
+                    <li className="headerItem">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="headerItem">
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li className="headerItem">
+                        <Link to="/404">404</Link>
+                    </li>
                 </ul>
             </nav>
             <Outlet />
--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,5 +1,20 @@
+import me from '../assets/me.png'
+
 const Home = () => {
-    return <h1> test </h1>;
+    return (
+        <>
+            <div className="home">
+                <div className="box">
+                    <img src={me} className="image"  alt='Ryan Cole'/>
+                    <div>
+                        <div className="titleText">Ryan Cole</div>
+                        <h2>Undergraduate Student studying</h2>
+                        <h2>Computer Science and Mathematics @ <a href="https://rpi.edu/">RPI</a></h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home;
